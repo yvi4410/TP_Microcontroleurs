ae6a596a83f2a84253bca2b94272f7eed1c19e25
Yvi Destombes <yvidestombes@MacBook-Pro-de-Yvi.local>
2025-01-24 09:38:17 +0100

    First commit

---
+++ TP1a_helloworld.X/Ressources/main.c
@@ -0,0 +1,10 @@
+#include "configbits.h" // Bits de configuration
+#include <xc.h>         // Definition des registres specifiques au uC
+
+void main(void) {
+    /* Code d'initialisation */
+
+    while(1){
+        /* Code a executer dans une boucle infinie */
+    }
+}
