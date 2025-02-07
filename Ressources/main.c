ab61a06b077b7c700e678b265116fa7b868428ec
yvi4410 <yvi.destombes@icloud.com>
2025-02-07 14:27:17 +0100

    ajout de ce qui a été fait

--- Ressources/main.c
+++ Ressources/main.c
@@ -1,10 +1,29 @@
 #include "configbits.h" // Bits de configuration
-#include <xc.h>         // Definition des registres specifiques au uC
+#include <xc.h>
+
+
+//config du délais
+void delai_approx(void) {
+    for (unsigned long i = 0; i < 50000; i++) {
+        __asm("nop");
+    }
+}
+
 
 void main(void) {
-    /* Code d'initialisation */
+    // Configuration des ports pour les LEDs
+    TRISD = 0x00;// Port D configuré en sortie
+    TRISB = 0x00;// Port B configuré en sortie
+    LATD = 0x00; // Initialisation à 0 (LEDs éteintes)
+    LATB = 0x00; // Initialisation à 0 (LEDs éteintes)
 
-    while(1){
-        /* Code a executer dans une boucle infinie */
+    while (1) {
+        LATD = 0xFF; 
+        delai_approx();
+        LATD = 0x00;
+
+        LATB = 0xFF;  
+        delai_approx();
+        LATB = 0x00;
     }
 }
