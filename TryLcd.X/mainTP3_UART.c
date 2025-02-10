5940d200222e1ab79ede785398ec21b5462e0925
Loan <loan.benedetto@gmail.com>
2025-02-10 16:43:34 +0100

    Hello world UART

--- TryLcd.X/mainTP3_UART.c
+++ TryLcd.X/mainTP3_UART.c
@@ -1,17 +1,19 @@
 #include "configbits.h" // Bits de configuration
 #include <xc.h>
 
-#define _XTAL_FREQ 8000000 // Frequence de l'oscillateur en Hertz
+#define _XTAL_FREQ 8000000 // Fréquence de l'oscillateur en Hertz
 
+// Fonction pour envoyer un caractère sur l'UART
 void UART_SendChar(char c) {
     while(!TX1STAbits.TRMT); // Attendre que l'UART soit prêt à envoyer
-    TX1REG = c;  // Envoyer un caractère
+    TX1REG = c;              // Envoyer le caractère
 }
 
-void UART_SendString(char* str) {
-    while(str) {
-        UART_SendChar(str);
-        str++;
+// Nouvelle fonction qui envoie exactement 'length' caractères depuis le tableau
+void UART_SendTable(const char *table, unsigned int length) {
+    unsigned int i;
+    for(i = 0; i < length; i++){
+         UART_SendChar(table[i]);
     }
 }
 
@@ -25,26 +27,30 @@ void setupUART() {
     ANSELCbits.ANSC6 = 0;  // Désactiver mode analogique sur RC6
     ANSELCbits.ANSC7 = 0;  // Désactiver mode analogique sur RC7
 
-    TX1STAbits.SYNC = 0;   // Mode Asynchrone
+    TX1STAbits.SYNC = 0;   // Mode asynchrone
     TX1STAbits.BRGH = 1;   // Baud Rate High Speed
     BAUD1CONbits.BRG16 = 0; // Utilisation de SP1BRGL uniquement
 
-    SP1BRGL = 12;  // Calculé pour 9600 bauds avec Fosc = 8MHz TODO a changer pcq pas sur
+    SP1BRGL = 51;  // Valeur calculée pour 9600 bauds avec Fosc = 8MHz
 
     RC1STAbits.SPEN = 1;  // Activer le module UART
     TX1STAbits.TXEN = 1;  // Activer la transmission
     RC1STAbits.CREN = 1;  // Activer la réception continue
-
 }
-
 
 // Fonction principale
 void main(void) {
     // Configuration UART
     setupUART();
 
+    // Définition du tableau de caractères à envoyer
+    const char message[] = "Hello, World!\n\r";
+    // Calcul de la longueur du message (sans compter le caractère nul)
+    unsigned int message_length = sizeof(message) - 1;
+
     while(1) {
-        UART_SendString("Hello, World!\n\r");
+        // Envoyer le contenu du tableau et s'arrêter après le dernier caractère
+        UART_SendTable(message, message_length);
         __delay_ms(1000);
     }
 }
